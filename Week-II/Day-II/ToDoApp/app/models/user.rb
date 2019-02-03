2a437cf8420d2b4d6d628c55582a8f65851f6539
railsfactory-pavan <pavanrf07@gmail.com>
2019-02-03 14:06:29 +0530

    no name

--- Week-II/Day-II/ToDoApp/app/models/user.rb
+++ Week-II/Day-II/ToDoApp/app/models/user.rb
@@ -2,22 +2,20 @@ class User < ApplicationRecord
   has_many :todos, dependent: :destroy
 
   has_secure_password
+
   validate :verify_unique_name , :verify_unique_email
+  validates_presence_of :name
+  validates_length_of :password, minimum: 6
 
-     validates_presence_of :name
-    
-     validates_length_of :password, minimum: 6
-     def verify_unique_name
-       if User.exists? name: name
-         errors.add :name, "has already been taken"
-       end
-     end
+  def verify_unique_name
+    if User.exists? name: name
+      errors.add :name, "has already been taken"
+    end
+  end
 
-     def verify_unique_email
-       if User.exists? email: email
-         errors.add :email, "has already been taken"
-       end
-     end
-
-
+  def verify_unique_email
+    if User.exists? email: email
+      errors.add :email, "has already been taken"
+    end
+  end
 end
