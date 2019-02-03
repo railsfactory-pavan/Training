9321ff50a505b285cbf818219d75f28a33f15b87
railsfactory-pavan <pavanrf07@gmail.com>
2019-02-03 14:25:54 +0530

    on my machine

--- Assignments/Assignments-II/Blog/db/schema.rb
+++ Assignments/Assignments-II/Blog/db/schema.rb
@@ -48,32 +48,6 @@ ActiveRecord::Schema.define(version: 20180301091219) do
     t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
   end
 
-  create_table "models", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.integer "failed_attempts", default: 0, null: false
-    t.string "unlock_token"
-    t.datetime "locked_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["confirmation_token"], name: "index_models_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_models_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
-    t.index ["unlock_token"], name: "index_models_on_unlock_token", unique: true
-  end
-
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
